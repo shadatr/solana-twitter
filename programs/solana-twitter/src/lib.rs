25c3edf0220a50e57f5a8b5dcc92bd6a4131931c
shdab <130516923+shadatr@users.noreply.github.com>
2024-01-26 14:36:18 +0300

    added tweets

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,16 +1,80 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+use std::convert::Into;
 
 declare_id!("EfpUgYt1dqvkdG2mneNUeDwW8PB9NBuVxFqMfK5vkgjG");
 
 #[program]
 pub mod solana_twitter {
+    
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get().unwrap();
+
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into());
+        }
+    
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into())
+        }
+    
+        tweet.author = *author.key;
+        tweet.timestamp = clock.unix_timestamp;
+        tweet.topic = topic;
+        tweet.content = content;
+    
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SendTweet<'info> {
+    #[account(init, payer = author, space = Tweet::LEN)]
+    pub tweet: Account<'info, Tweet>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+#[account]
+pub struct Tweet {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub topic: String,
+    pub content: String,
+}
 
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4; 
+const MAX_TOPIC_LENGTH: usize = 50 * 4; 
+const MAX_CONTENT_LENGTH: usize = 280 * 4; 
+
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH 
+        + TIMESTAMP_LENGTH 
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH 
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; 
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
+}
+
+impl From<ErrorCode> for ProgramError {
+    fn from(error: ErrorCode) -> Self {
+        match error {
+            ErrorCode::TopicTooLong => ProgramError::Custom(1), // Replace with the actual error code
+            ErrorCode::ContentTooLong => ProgramError::Custom(2), // Replace with the actual error code
+        }
+    }
+}
--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaTwitter } from "../target/types/solana_twitter";
+import * as assert from "assert";
 
 describe("solana-twitter", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,53 @@ describe("solana-twitter", () => {
 
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it('can send a new tweet', async () => {
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('veganism', 'Hummus, am I right?', {
+        accounts: {
+            tweet: tweet.publicKey,
+            author: anchor.AnchorProvider.env().wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [tweet],
+    });
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+  	assert.equal(tweetAccount.author.toBase58(), anchor.AnchorProvider.env().wallet.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, 'veganism');
+    assert.equal(tweetAccount.content, 'Hummus, am I right?');
+    assert.ok(tweetAccount.timestamp);
+});
+
+  it('can send a new tweet from a different author', async () => {
+    // Generate another user and airdrop them some SOL.
+    const otherUser = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature);
+
+    // Call the "SendTweet" instruction on behalf of this other user.
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('veganism', 'Yay Tofu!', {
+        accounts: {
+            tweet: tweet.publicKey,
+            author: otherUser.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [otherUser, tweet],
+    });
+
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+    // Ensure it has the right data.
+    console.log(tweetAccount);
+    assert.equal(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, 'veganism');
+    assert.equal(tweetAccount.content, 'Yay Tofu!');
+    assert.ok(tweetAccount.timestamp);
   });
+
+  it('can fetch all tweets', async () => {
+    const tweetAccounts = await program.account.tweet.all();
+    assert.equal(tweetAccounts.length, 9);
+});
 });
